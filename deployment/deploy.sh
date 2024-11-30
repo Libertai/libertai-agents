7efd24b36aec9af1a3870268fb6dd58417608200
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-12-01 03:15:16 +0900

    feat(deployment): Basic script working

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Setup
-apt install docker.io -y
+apt install docker.io unzip -y
 
 # Cleaning previous agent
 rm -rf /root/libertai-agent
@@ -9,9 +9,9 @@ docker stop libertai-agent && docker rm $_
 
 # Deploying the new agent
 unzip /tmp/libertai-agent.zip -d /root/libertai-agent
-wget https://TODO -o /tmp/libertai-agent.Dockerfile
+wget https://raw.githubusercontent.com/Libertai/libertai-agents/refs/heads/reza/deployment-instances/deployment/$2.Dockerfile -O /tmp/libertai-agent.Dockerfile -q
 docker build /root/libertai-agent \
   -f /tmp/libertai-agent.Dockerfile \
   -t libertai-agent \
   --build-arg PYTHON_VERSION=$1
-docker run -p 8000:8000 libertai-agent
+docker run --name libertai-agent -p 8000:8000 -d libertai-agent
