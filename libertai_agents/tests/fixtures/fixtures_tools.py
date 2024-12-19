f9fd9ac1d18f53bacd47a64e376d578c5ff03b9d
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-12-19 20:04:11 +0900

    test: Python 3.10+ union-style param in function tool

--- libertai_agents/tests/fixtures/fixtures_tools.py
+++ libertai_agents/tests/fixtures/fixtures_tools.py
@@ -18,3 +18,20 @@ def fake_get_temperature_tool() -> Callable:
         return 22.0  # A real function should probably actually get the temperature!
 
     return get_current_temperature
+
+
+@pytest.fixture()
+def fake_get_temperature_with_new_union_style_param_tool() -> Callable:
+    def get_current_temperature(location: str, unit: str | None) -> float:
+        """
+        Get the current temperature at a location.
+
+        Args:
+            location: The location to get the temperature for, in the format "City, Country"
+            unit: The unit to return the temperature in. (choices: ["celsius", "fahrenheit"])
+        Returns:
+            The current temperature at the specified location in the specified units, as a float.
+        """
+        return 22.0  # A real function should probably actually get the temperature!
+
+    return get_current_temperature
--- libertai_agents/tests/tools/test_function_tools.py
+++ libertai_agents/tests/tools/test_function_tools.py
@@ -6,4 +6,18 @@ def test_function_example_tool(fake_get_temperature_tool):
     assert libertai_tool.name == fake_get_temperature_tool.__name__
 
 
+def test_function_new_union_style_param_tool(
+    fake_get_temperature_with_new_union_style_param_tool,
+):
+    libertai_tool = Tool.from_function(
+        fake_get_temperature_with_new_union_style_param_tool
+    )
+    print(libertai_tool.args_schema)
+    assert (
+        libertai_tool.name
+        == fake_get_temperature_with_new_union_style_param_tool.__name__
+    )
+    assert libertai_tool.args_schema
+
+
 # TODO: add test with Python 3.10+ union style when https://github.com/huggingface/transformers/pull/35103 merged + new release
